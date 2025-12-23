5adcd781c340864ce0d009b3502272fb5c8b8641
Ansul Agrawal <ansul.developer@gmail.com>
2025-12-23 16:41:58 +0530

    fix: update avatar image URL to use a hosted link and remove local avatar file

--- src/helpers/Helper.ts
+++ src/helpers/Helper.ts
@@ -2,5 +2,8 @@
  * Get dynamic image path
  */
 export function getImagePath(image: string) {
+  if (image.startsWith('http')) {
+    return image;
+  }
   return new URL(`../assets/${image}`, import.meta.url).href;
 }
